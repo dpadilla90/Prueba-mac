a3bcf5e0656cb91e775a6b543067a729e5af9e6f
dpadilla90 <d_padilla.y@hotmail.com>
2023-05-11 21:02:06 -0400

    Initial Commit

---
+++ prueba/pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Daphne Rivera on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
